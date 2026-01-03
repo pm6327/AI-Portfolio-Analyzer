576b7851a3595decdd7b83b2b790ccef2f85a219
Prakhar Maheshwari <prakharmaheshwari33@gmail.com>
2026-01-03 15:42:23 +0530

    title changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,52 +2,39 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="TTF",
+    page_title="AI Protfolio Analyzer",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'About': 'A personal project by abhaypai@vt.edu',
-    }
+        "About": "A personal project by abhaypai@vt.edu",
+    },
 )
 
 input_page = st.Page(
-    page = "Pages/1_Set Profile.py",
-    title = "Input",
-    default = True,
+    page="Pages/1_Set Profile.py",
+    title="Input",
+    default=True,
 )
 
-charts_page = st.Page(
-    page = "Pages/2_Stock Charts.py",
-    title = "Stock Charts"
-)
+charts_page = st.Page(page="Pages/2_Stock Charts.py", title="Stock Charts")
 
-risk_page = st.Page(
-    page = "Pages/3_Risk Analysis.py",
-    title = "Risk Analysis"
-)
+risk_page = st.Page(page="Pages/3_Risk Analysis.py", title="Risk Analysis")
 
-optimal_page = st.Page(
-    page = "Pages/4_Optimal Portfolio.py",
-    title = "Optimal Portfolio"
-)
+optimal_page = st.Page(page="Pages/4_Optimal Portfolio.py", title="Optimal Portfolio")
 
 talk_to_p = st.Page(
-    page = "Pages/5_Talk To Portfolio.py",
-    title = "Chat with Portfolio",
-    icon=":material/thumb_up:"
+    page="Pages/5_Talk To Portfolio.py",
+    title="Chat with Portfolio",
+    icon=":material/thumb_up:",
 )
 
 
-inputs=[input_page]
-outputs=[charts_page, risk_page, optimal_page]
+inputs = [input_page]
+outputs = [charts_page, risk_page, optimal_page]
 chat_page = [talk_to_p]
 
 pg = st.navigation(
-    {
-        "User Input" : inputs,
-        "Analyses" : outputs,
-        "Chat with your Portfolio" :chat_page
-    }
+    {"User Input": inputs, "Analyses": outputs, "Chat with your Portfolio": chat_page}
 )
 
 st.sidebar.link_button("See Code Here:", "https://streamlit.io/gallery")
